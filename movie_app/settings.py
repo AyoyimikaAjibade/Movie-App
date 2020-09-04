96b7f68f176e42bdde1e8ab03102ca09eadaf409
Ayoyimika Ajibade <ajibadeayoyimika@gmail.com>
2020-09-04 17:00:12 +0100

    Add files via upload

--- movie_app/settings.py
+++ movie_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '@w%t(=%mt2wp#d+^&4350g39y$1i2i#k$-1m(_5nt-vww4n)f7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ayoyimika-movieapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -117,5 +117,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR. 'staticfiles')
 STATIC_URL = '/static/'
