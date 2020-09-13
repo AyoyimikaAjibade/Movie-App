5ec0b08ba6d85e2d63aa0bc5acdd64db83e33be1
Ayoyimika Ajibade <ajibadeayoyimika@gmail.com>
2020-09-13 22:42:08 +0100

    deployments

--- Movie_App/settings.py
+++ Movie_App/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '-kpl(n-&%&!r#0avrxr#k4oju23fn@be1pa-o0ib7g&f(2u6pp'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ayoyimika-movieapp.herokuapp.com', '127.0.0.1']
 
